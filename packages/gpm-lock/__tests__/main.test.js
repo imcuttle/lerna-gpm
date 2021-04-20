fab90b6cdceb5198efbf3a08c72ad63bf95d52b4
余聪 <yucong@yuanfudao.com>
2021-04-20 17:16:34 +0800

    chore: gpm-lock

--- packages/gpm-lock/__tests__/main.test.js
+++ packages/gpm-lock/__tests__/main.test.js
@@ -12,7 +12,7 @@ const exec = (cmd) => {
   return execSync(cmd.replace('lerna', nps.join(__dirname, '../../../node_modules/.bin/lerna')), {
     encoding: 'utf8',
     cwd: fixture()
-  })
+  }).trim()
 }
 
 beforeAll(() => {
