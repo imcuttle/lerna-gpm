5c36a5c3a897bbc5f285db641259128904ca8db8
余聪 <yucong@yuanfudao.com>
2021-04-21 17:08:14 +0800

    chore: gpm-lock

--- packages/gpm-pull/__tests__/main.test.js
+++ packages/gpm-pull/__tests__/main.test.js
@@ -2,6 +2,11 @@ const gpmPull = require('../src')
 const nps = require('path')
 const { execSync } = require('child_process')
 const { fixture } = require('./helper')
+const { readFileSync, writeFileSync } = require('fs')
+
+const readLernaJson = () => {
+  return JSON.parse(readFileSync(fixture('lerna.json')).toString())
+}
 
 const exec = (cmd) => {
   return execSync(cmd.replace('lerna', nps.join(__dirname, '../../../node_modules/.bin/lerna')), {
@@ -18,17 +23,42 @@ afterAll(() => {
   exec('rm -rf packages/tmp')
 })
 
-describe('gpmPull', function() {
-  it(
-    'spec case',
-    function () {
-      const head = exec('cd packages/tmp && git rev-parse HEAD')
-      exec('cd packages/tmp && git reset HEAD~1 --hard')
-      const headLastOne = exec('cd packages/tmp && git rev-parse HEAD')
-      expect(headLastOne).not.toBe(head)
+describe('gpmPull', function () {
+  it('spec case', function () {
+    const head = exec('cd packages/tmp && git rev-parse HEAD')
+    exec('cd packages/tmp && git reset HEAD~1 --hard')
+    const headLastOne = exec('cd packages/tmp && git rev-parse HEAD')
+    expect(headLastOne).not.toBe(head)
 
-      exec('lerna gpm-pull packages/tmp')
-      expect(exec('cd packages/tmp && git rev-parse HEAD')).toBe(head)
-    }
-  )
+    exec('lerna gpm-pull packages/tmp')
+    expect(exec('cd packages/tmp && git rev-parse HEAD')).toBe(head)
+  })
+
+  it('spec case --lock', function () {
+    const head = exec('cd packages/tmp && git rev-parse HEAD')
+    exec('cd packages/tmp && git reset HEAD~1 --hard')
+
+    const lernaConfig = readLernaJson()
+    writeFileSync(
+      fixture('lerna.json'),
+      JSON.stringify(
+        {
+          ...lernaConfig,
+          gpm: {
+            ...lernaConfig.gpm,
+            'packages/tmp': {
+              ...lernaConfig.gpm['packages/tmp'],
+              checkout: null
+            }
+          }
+        },
+        null,
+        2
+      )
+    )
+
+    exec('lerna gpm-pull packages/tmp --lock')
+    expect(exec('cd packages/tmp && git rev-parse HEAD')).toBe(head)
+    expect(readLernaJson().gpm['packages/tmp']).toMatchInlineSnapshot()
+  })
 })
